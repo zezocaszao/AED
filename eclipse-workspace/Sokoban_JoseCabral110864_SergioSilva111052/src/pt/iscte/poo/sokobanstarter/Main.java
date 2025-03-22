90eef829c3a5b1748a792e9a18bcb208662a127f
zezocaszao <jpcac1@iscte-iul.pt>
2025-03-22 10:26:37 +0000

    Delete eclipse-workspace/Sokoban_JoseCabral110864_SergioSilva111052 directory

--- eclipse-workspace/Sokoban_JoseCabral110864_SergioSilva111052/src/pt/iscte/poo/sokobanstarter/Main.java
+++
@@ -1,9 +0,0 @@
-package pt.iscte.poo.sokobanstarter;
-
-public class Main { //main
-
-	public static void main(String[] args) {
-		GameEngine.getInstance().start();
-	}
-
-}
--- eclipse-workspace/Sokoban_JoseCabral110864_SergioSilva111052/src/pt/iscte/poo/sokobanstarter/Movable.java
+++
@@ -1,8 +0,0 @@
-package pt.iscte.poo.sokobanstarter;
-
-import pt.iscte.poo.utils.Point2D;
-
-public interface Movable {
-	public void move(int Key);//mexe
-	public boolean isMovable(Point2D newBobcatPosition, int Key);//Booleano que define se é possível mexer.
-}
